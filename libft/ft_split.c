a221162f940ffe96968017aea486bd46411de91b
Antoine Mouly <amouly@k0r4p7.42mulhouse.fr>
2023-03-01 18:03:24 +0100

    libft + new split

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -3,120 +3,167 @@
 /*                                                        :::      ::::::::   */
 /*   ft_split.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: llion <llion@student.42mulhouse.fr >       +#+  +:+       +#+        */
+/*   By: amouly <amouly@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/07 17:36:49 by llion             #+#    #+#             */
-/*   Updated: 2023/03/01 17:28:09 by llion            ###   ########.fr       */
+/*   Created: 2022/07/29 08:04:32 by amouly            #+#    #+#             */
+/*   Updated: 2023/03/01 17:26:58 by amouly           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include <stdlib.h>
 
-static int	count_words(char const *s, char c)
+int put_flag(char c, int flag)
+{
+	if (flag == 0 && c == '"')
+		return (1);
+	else if (flag == 0 && c == '\'')
+		return (2);
+	else if (flag == 1 && c == '"')
+		return (0);
+	else if (flag == 2 && c == '\'')
+		return (0);
+	else 
+		return (flag);
+}
+
+
+int	count_word(char const *str, char c)
 {
 	int	i;
 	int	count;
+	int flag;
 
-	i = 0;
+	flag = 0;
 	count = 0;
-	while (s[i])
+	i = 0;
+	while (str[i] != '\0' && str[i] == c)
+		i++;
+	
+	while (str[i] != '\0')
 	{
-		if (i == 0 && s[i] != c)
+		flag = put_flag(str[i], flag);
+		while (str[i] != '\0' && str[i] != c && flag == 0)
 		{
-			count++;
 			i++;
+			flag = put_flag (str[i], flag);
 		}
-		else if (s[i] != c && s[i - 1] == c)
+		
+		if ((str[i] == '\0' || str[i] == c )&& flag == 0)
+			count++;
+		while (str[i] != '\0' && str[i] == c && flag == 0 )
 		{
-			count++;
 			i++;
+			flag = put_flag(str[i], flag);
 		}
-		else
+		if (flag == 1 || flag == 2)
 			i++;
 	}
 	return (count);
 }
 
-static char	**malloc_error(char **tab, unsigned int j)
-{
-	unsigned int	i;
-
-	i = 0;
-	while (i < j)
-	{
-		free(tab[i]);
-		i++;
-	}
-	free(tab);
-	return (NULL);
-}
-
-static int	word_len(char *s, char c, int flag)
+/*int	count_word(char const *str, char c)
 {
 	int	i;
 	int	count;
 
-	i = 0;
 	count = 0;
-	while (s[i] == c)
+	i = 0;
+	while (str[i] != '\0' && str[i] == c)
 		i++;
-	while ((s[i] != c && s[i] != 0) || flag == 1 || flag == 2)
+	
+	while (str[i] != '\0')
 	{
-		if (flag == 1 && s[i] == '"')
-			break ;
-		if (flag == 2 && s[i] == '\'')
-			break ;
+		while (str[i] != '\0' && str[i] != c )
+			i++;
+
+		if ((str[i] == '\0' || str[i] == c ))
+			count++;
+		while (str[i] != '\0' && str[i] == c )
+			i++;
+		
+	}
+	return (count);
+}*/
+
+int	long_mot(char const *str, char c, int flag)
+{
+	int	count;
+	int	i;
+
+	count = 0;
+	i = 0;
+	while (str[i] == '\0' || str[i] == c)
+		return (count);
+	while ((str[i] != '\0' && str[i] != c) || flag == 1 || flag == 2)
+	{
+		if (flag == 1 && str[i] == '"' )
+			break;
+		if (flag == 2 && str[i] == '\'' )
+			break;
 		i++;
 		count++;
 	}
 	return (count);
 }
 
-static char	*fill_tab(char *s, char c)
+int	free_tab(char **tab, int i)
+{
+	int	j;
+
+	j = 0;
+	while (j < i)
+	{
+		free(tab[j]);
+		j++;
+	}
+	free(tab);
+	return (0);
+}
+
+int	fill_tab(char **tab, char c, char const *s)
 {
 	int		i;
-	char	*word;
-	int		word_lenght;
-
+	int		pos;
+	int		j;
+	int 	flag;
+	
+	flag = 0;
 	i = 0;
-	word_lenght = word_len(s, c, flag);
-	word = malloc(sizeof(char) * (word_lenght + 1));
-	if (word == 0)
-		return (NULL);
-	while (i < word_lenght)
+	pos = 0;
+	while (i < count_word(s, c))
 	{
-		word[i] = s[i];
-		i++;
+		flag = put_flag(s[pos], flag);
+		while (long_mot(&s[pos], c, flag) == 0)
+			pos++;
+		tab[i] = malloc (sizeof(char) * (long_mot(&s[pos], c, flag) + 1));
+		if (tab[i] == NULL)
+			return (free_tab(tab, i));
+		j = 0;
+		while (((s[pos] != c) && s[pos] != '\0') || flag == 1 || flag == 2 )
+		{	
+			flag = put_flag(s[pos], flag);
+			tab[i][j] = s[pos];
+			pos++;
+			j++;
+		}
+	tab[i][j] = '\0';
+	i++;
 	}
-	word[i] = 0;
-	return (word);
+	tab[i] = 0;
+	return (1);
 }
 
 char	**ft_split(char const *s, char c)
 {
 	char	**tab;
-	int		tab_len;
-	int		i;
-	int		j;
 
-	i = 0;
-	j = 0;
-	tab_len = count_words(s, c);
-	tab = malloc(sizeof(char *) * (tab_len + 1));
-	if (tab == 0)
-		return (0);
-	while (*s)
-	{
-		if ((i == 0 && s[i] != c) || (*s != c && *(s - 1) == c))
-		{
-			tab[j] = fill_tab((char *)s, c);
-			if (tab[j] == NULL)
-				return (malloc_error(tab, j));
-			i++;
-			j++;
-		}
-		s++;
-	}
-	tab[j] = 0;
-	return (tab);
+	if (s == NULL)
+		return (NULL);
+	tab = malloc(sizeof(char *) * (count_word(s, c) + 1));
+	if (tab == NULL)
+		return (NULL);
+	if (fill_tab(tab, c, s) == 0)
+		return (NULL);
+	else
+		return (tab);
 }
