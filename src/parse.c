d059f7cc4d7cfe1b14b57b888f8b90cc9c7ee01e
Antoine Mouly <amouly@k0r4p7.42mulhouse.fr>
2023-03-05 13:19:51 +0100

    split fonctionnel. A bien tester

--- src/parse.c
+++ src/parse.c
@@ -6,27 +6,27 @@
 /*   By: amouly <amouly@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/29 08:04:32 by amouly            #+#    #+#             */
-/*   Updated: 2023/03/04 15:49:25 by amouly           ###   ########.fr       */
+/*   Updated: 2023/03/05 13:17:44 by amouly           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include <stdio.h>
 
-// Countword fonctionne faire a partir de cela
-void put_flag(char c, int *flag)
+// TOUT FONCTIONNE A TESTER //
+
+int put_flag(char c, int flag)
 {
-	if (*flag == 0 && c == '"')
-		*flag = 1;
-	else if (*flag == 0 && c == '\'')
-		*flag = 2;
-	else if (*flag == 1 && c == '"')
-		*flag = 0;
-	else if (*flag == 2 && c == '\'')
-		*flag = 0;
+	if (flag == 0 && c == '"')
+		return(1);
+	else if (flag == 0 && c == '\'')
+		return(2);
+	else if (flag == 1 && c == '"')
+		return(0);
+	else if (flag == 2 && c == '\'')
+		return(0);
 	else 
-		*flag = *flag;
-	
+		return flag;
 }
 
 int	count_word_ms(char const *str)
@@ -42,18 +42,18 @@ int	count_word_ms(char const *str)
 		i++;
 	while (str[i] != '\0')
 	{
-		put_flag(str[i], &flag);
+		flag = put_flag(str[i], flag);
 		while (str[i] != '\0' && str[i] != ' ' && flag == 0)
 		{
 			i++;
-			put_flag(str[i], &flag);
+			flag = put_flag(str[i], flag);
 		}
 		if ((str[i] == '\0' || str[i] == ' ' )&& flag == 0)
 			count++;
 		while (str[i] != '\0' && str[i] == ' ' && flag == 0)
 		{
 			i++;
-			put_flag(str[i], &flag);
+			flag = put_flag(str[i], flag);
 		}
 		if (flag == 1 || flag == 2)
 			i++;
@@ -61,41 +61,60 @@ int	count_word_ms(char const *str)
 	return (count);
 }
 
-int	len_word(char const *str)
+int	len_word(char const *str, int *pos)
 {
-	int	count;
-	int	i;
-	int	flag;
+	int count;
+	int flag;
+	int flag1;
 
+	count  = 0;
 	flag = 0;
-	count = 0;
-	i = 0;
-	if ((str[i] == '\0' || str[i] == ' '))
-		return (count);
-	put_flag(str[i], &flag);
-	while ((str[i] != '\0' && str[i] != ' ') || flag == 1 || flag == 2)
+	flag1 = 0;
+	while ((str[*pos] == '\0' || str[*pos] == ' '))
+		(*pos)++;
+	while ((str[*pos] != '\0' && str[*pos] != ' ') || flag == 1 || flag == 2)
 	{
-		if (flag == 1 && str[i] == '"' )
-		{
-			flag = 0;
-			i++;
-		}
-		else if (flag == 2 && str[i] == '\'' )
-		{
-			flag = 0;
-			i++;
-		}
-		else 
-		{
-			i++;
-			count++;
-		}
-		put_flag(str[i], &flag);
+		flag1 = put_flag(str[*pos], flag);
+		if ((flag1 == 1 && flag != 1)|| (flag1 == 2 && flag !=2) || (flag1 == 0 && flag!= 0))
+			(*pos)++;
+		else
+			{
+				count++;
+				(*pos)++;
+			}
+		flag = flag1;
 	}
 	return (count);
 }
 
-/*int	free_tab(char **tab, int i)
+void	put_word(char const *str, int *pos, char *line_tab)
+{
+	int i;
+	int flag;
+	int flag1;
+
+	i = 0;
+	flag = 0;
+	flag1 = 0;
+	while ((str[*pos] == '\0' || str[*pos] == ' '))
+		(*pos)++;
+	while ((str[*pos] != '\0' && str[*pos] != ' ') || flag == 1 || flag == 2)
+	{
+		flag1 = put_flag(str[*pos], flag);
+		if ((flag1 == 1 && flag != 1)|| (flag1 == 2 && flag !=2) || (flag1 == 0 && flag!= 0))
+			(*pos)++;
+		else
+			{
+				line_tab[i] = str[*pos]; 
+				(*pos)++;
+				i++;
+			}
+		flag = flag1;
+	}
+	line_tab[i] = '\0';
+}
+
+int	free_tab_ms_split(char **tab, int i)
 {
 	int	j;
 
@@ -108,54 +127,37 @@ int	len_word(char const *str)
 	free(tab);
 	return (0);
 }
-*/
-int	fill_tab_parse(int **tab, char const *s)
+
+int	fill_tab_parse(char **tab, char const *s)
 {
 	int		i;
 	int		pos;
-	//int		j;
+	int		pos_word;
+	int		len;
 	
 	i = 0;
 	pos = 0;
 	while (i < count_word_ms(s))
 	{
-		while (len_word(&s[pos]) == 0)
-			pos++;
-		/*tab[i] = malloc (sizeof(char) * (long_mot(&s[pos] c, flag) + 1));
-		//printf("len: %d\n", long_mot(&s[pos], c, flag));
+		pos_word = pos;
+		len = len_word(s, &pos);
+		tab[i] = malloc (sizeof(char) * (len + 1));
 		if (tab[i] == NULL)
-			return (free_tab(tab, i));
-		j = 0;
-		int len = long_mot(&s[pos], c, flag);
-		//while (((s[pos] != c) && s[pos] != '\0') || flag == 1 || flag == 2) 
-		while (s[pos] != '\0'&& j < len) 
-		{	
-			flag = put_flag(s[pos], flag);
-			if ((flag == 1 || flag == 2) && flag2 == 0)	
-			{
-				tab[i][j] = s[++pos];
-				flag2 = 1;
-			}
-			else 
-				tab[i][j] = s[pos];
-			pos++;
-			j++;
-		}
-		tab[i][j] = '\0';*/
-		*tab[i] = len_word(&s[pos]);
+			return (free_tab_ms_split(tab, i));
+		put_word(s, &pos_word, tab[i]);
 		i++;
 	}
 	tab[i] = 0;
 	return (1);
 }
 
-int	*ft_split_int(char const *s)
+char	**ft_split_ms(char const *s)
 {
-	int	*tab;
+	char	**tab;
 
 	if (s == NULL)
 		return (NULL);
-	tab = malloc(sizeof( *int) * (count_word_ms(s) + 1));
+	tab = malloc(sizeof( int*) * (count_word_ms(s) + 1));
 	if (tab == NULL)
 		return (NULL);
 	if (fill_tab_parse(tab, s) == 0)
@@ -166,13 +168,14 @@ int	*ft_split_int(char const *s)
 
 void split_and_print(char *line)
 {
-	int **tab;
+	char **tab;
 	int i = 0;
 	
-	tab = ft_split_int(line);
+	tab = ft_split_ms(line);
 	while (tab[i])
 	{
-		printf("tab[%d] : %d\n", i, *tab[i]);
+		printf("tab[%d] : %s\n", i, tab[i]);
 		i++;
 	}
+	printf("tab[%d] : %s\n", i, tab[i]);
 }
