ae5f99da054bfbb28f7c38bc5a72de02ab0eb742
Antoine Mouly <amouly@k0r4p7.42mulhouse.fr>
2023-03-14 10:59:50 +0100

    avant merge

--- src/main_antoine.c
+++ src/main_antoine.c
@@ -6,15 +6,8 @@ int main()
 
     while (1)
     {
-        //char    **tab;
         input = readline("Minishell> ");
         add_history(input);
-        //split_and_print(input);
-        //printf("afetr formating\n");
-        //tab = ft_split_ms(format_line(input));
-        //print_input_after_formating(input);
-        //print_input_after_formating(input);
-        //printf ("-------------------\n");
         parse_input(input);
     }
 }
