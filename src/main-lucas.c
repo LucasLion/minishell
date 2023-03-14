a46290a1c4debaa3b8c0e10af8cbfcdca7a8d1fd
Lucas Lion <llion@k0r1p3.42mulhouse.fr>
2023-03-14 13:08:15 +0100

    main-Lucas.c

--- src/main-lucas.c
+++ src/main-lucas.c
@@ -6,7 +6,7 @@
 /*   By: llion <llion@student.42mulhouse.fr >       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/09 14:55:57 by llion             #+#    #+#             */
-/*   Updated: 2023/03/14 10:53:30 by llion            ###   ########.fr       */
+/*   Updated: 2023/03/14 13:07:31 by llion            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,11 @@ int main(int argc, char **argv, char **envp)
 	(void)argc;
 	(void)argv;
 	char	*line;
-	char **new;
 
 	while (1)
 	{
 		line = readline("Minishell$ ");
 		add_history(line);
-		char *string = "LANG token";
-		new = unset(envp, string);
-	for (int i = 0; new[i]; i++)
-		printf("%d: %s\n", i, new[i]);
+		 
 	}
 }
