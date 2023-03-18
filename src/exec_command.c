a169a30fcec8065679084448e782e8a175d87244
Lucas <lucaslion7@gmail.com>
2023-03-18 16:10:39 +0100

    baby shell is born

--- src/exec_command.c
+++ src/exec_command.c
@@ -6,7 +6,7 @@
 /*   By: llion <llion@student.42mulhouse.fr >       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/16 16:55:58 by llion             #+#    #+#             */
-/*   Updated: 2023/03/17 15:13:54 by llion            ###   ########.fr       */
+/*   Updated: 2023/03/18 16:10:06 by llion            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,19 +33,45 @@ char	**get_path(char **envp)
 	return (split_path);
 }
 
-
-
-int	exec_command(char *command, char **argv, char **envp)
+char	*get_command(t_command *list, char **envp)
 {
-	size_t	exit_status;
+	char	*cmd;
+	char	*pre_cmd;
+	char	**path;
+	int	i;
+
+	i = 0;
+	path = get_path(envp);
+	pre_cmd = NULL;
+	cmd = NULL;
+	while (path[i])
+	{
+		pre_cmd = ft_strjoin(path[i], "/");
+		cmd = ft_strjoin(pre_cmd, list->command->string);
+		if (access(cmd, F_OK) == 0)
+			return (cmd);
+		i++;
+	}
+	return (cmd);
+}
+
+int	exec_command(char **argv, char **envp, t_command *list, char *line)
+{
+	(void)argv;
+	(void)envp;
+	int	exit_status;
 	int		status;
+	char	*cmd;
+
 	pid_t	pid;
-	//char	**path;
-    (void)command;
 	pid = fork();
-	//path = get_path(envp);
 	if (pid == 0)
-		exit_status = execve("/bin/df", argv, envp);
+	{
+		parse_input(line, &list);
+		add_history(line);
+		cmd = get_command(list, envp);
+		exit_status = execve(cmd, argv, envp);
+	}
 	else if (pid < 0)
 		return (EXIT_FAILURE);
 	else
